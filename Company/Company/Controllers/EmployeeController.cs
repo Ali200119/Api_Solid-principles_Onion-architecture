ffc665922cd3f9022a7ae225e924dc263a05e38b
Ali200119 <aliit@code.edu.az>
2023-05-17 04:26:13 +0400

    Introduction

---
+++ Company/Company/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //[HttpGet]
+        //public string GetName()
+        //{
+        //    return "Ali";
+        //}
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new string[] { "Ali", "Cavid", "Cinare", "Elcan" });
+        }
+
+        [HttpGet]
+        [Route("Test/{id}")]
+        public IActionResult GetById(int? id)
+        {
+            if (id is null) return BadRequest();
+
+            return Ok("Ali - " + " " + id);
+        }
+    }
+}
